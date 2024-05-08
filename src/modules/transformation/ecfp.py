7f9d5015c5c19db6a461c029aa72da2580e30b4e
Ariel Ebersberger <ariel@ebersberger.io>
2024-05-08 15:54:12 +0200

    Fix pre-commits

--- src/modules/transformation/ecfp.py
+++ src/modules/transformation/ecfp.py
@@ -46,14 +46,14 @@ class ECFP(VerboseTransformationBlock):
         use_features: bool = False,
         progressbar_desc: str | None = None,
     ) -> npt.NDArray[np.uint8]:
-        """Worker function to process a single SMILES string.
+        """Worker function to process a batch of SMILES strings.
 
         :param smiles: A list of SMILES strings.
         :param radius: The radius of the ECFP.
         :param bits: The number of bits in the ECFP.
         :param use_features: Whether to use features in the ECFP.
         :param progressbar_desc: Description for the progress bar.
-        :return: A list of ECFP fingerprints or a list of dictionaries containing SMILES and ECFP pairs.
+        :return: A numpy array of fingerprints.
         """
         result = np.empty((len(smiles), bits // 8), dtype=np.uint8)
         iterator = tqdm(enumerate(smiles), desc=progressbar_desc) if progressbar_desc is not None else enumerate(smiles)
@@ -68,7 +68,7 @@ class ECFP(VerboseTransformationBlock):
 
         :param smile_array: A list of SMILES strings.
         :param desc: Description for logging purposes.
-        :return: A list of ECFP fingerprints or a list of dictionaries containing SMILES and ECFP pairs.
+        :return: A numpy array of fingerprints.
         """
         chunk_size = len(smile_array) // NUM_FUTURES
         chunk_size = max(chunk_size, MIN_CHUNK_SIZE)
