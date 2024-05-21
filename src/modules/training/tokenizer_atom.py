9fbb0162e8c91642a03c99d2c6d4aea62e06604f
Gregoire <dumont.gregoire.c@gmail.com>
2024-05-21 21:10:33 +0200

    Fixed pre-commit issues

--- src/modules/training/tokenizer_atom.py
+++ src/modules/training/tokenizer_atom.py
@@ -1,58 +1,70 @@
 """Train a torch tokenizer on the molecule smiles."""
 
+import pickle
+from dataclasses import dataclass
+
+import joblib
 import numpy as np
 import numpy.typing as npt
-from rdkit import Chem
-import joblib
-import pickle
+from torchnlp.encoders.text import StaticTokenizerEncoder  # type: ignore[import-not-found]
 from tqdm import tqdm
-from dataclasses import dataclass
-from src.typing.xdata import XData
-from torchnlp.encoders.text import StaticTokenizerEncoder
-from src.modules.training.verbose_training_block import VerboseTrainingBlock
 
-def identity(x):
+from src.modules.training.verbose_training_block import VerboseTrainingBlock
+from src.typing.xdata import XData
+
+
+def identity(x: list[str]) -> list[str]:
+    """Transform the molecule for the tokenizer.
+
+    :param x: list containing the tokens
+    :return: list containing the tokens
+    """
     return x
+
 
 @dataclass
 class TokenizerAtom(VerboseTrainingBlock):
     """Train a torch tokenizer on the molecule smiles."""
 
-    def apply_tokenizer(self, smiles: list[list[str]], encoder):
-        """ Apply the tokenizer on the smile molecules.
+    def apply_tokenizer(self, smiles: list[list[str]]) -> npt.NDArray[np.uint8]:
+        """Apply the tokenizer on the smile molecules.
 
         :param smiles: the segmented smile molecules
-        :param encoder: torch nlp tokenizer"""
-
-        return np.array([encoder.encode(smile) for smile in tqdm(smiles, desc="encode molecules")])
+        :param encoder: torch nlp tokenizer
+        """
+        return np.array([self.encoder.encode(smile) for smile in tqdm(smiles, desc="encode molecules")])
 
     def custom_train(self, X: XData, y: npt.NDArray[np.float32]) -> tuple[XData, npt.NDArray[np.float32]]:
         """Train the torch tokenizer on the sentences.
 
         :param X: XData containing the molecule smiles
         :param y: array containing the molecule labels
-        :return: The tokenized sentences and labels"""
+        :return: The tokenized sentences and labels
+        """
+        self.log_to_terminal("start training the tokenizer.")
 
-        self.log_to_terminal(f"start training the tokenizer.")
+        # Check whether the building blocks are present
+        if X.bb1_smiles is None or X.bb2_smiles is None or X.bb3_smiles is None:
+            raise ValueError("There is no SMILE information for the molecules")
 
         # Extract the smiles from each building block
         smiles = list(X.bb1_smiles) + list(X.bb2_smiles) + list(X.bb3_smiles)
 
         # Train the torch nlp tokenizer on the sequences
         tqdm_smiles = tqdm(smiles, desc="Tokenizing molecules")
-        encoder = StaticTokenizerEncoder(tqdm_smiles, tokenize=identity)
+        self.encoder = StaticTokenizerEncoder(tqdm_smiles, tokenize=identity)
 
         # Apply the torch nlp tokenizer on the building blocks
-        X.bb1_ecfp = self.apply_tokenizer(list(X.bb1_smiles), encoder)
-        X.bb2_ecfp = self.apply_tokenizer(list(X.bb2_smiles), encoder)
-        X.bb3_ecfp = self.apply_tokenizer(list(X.bb3_smiles), encoder)
+        X.bb1_ecfp = self.apply_tokenizer(list(X.bb1_smiles))
+        X.bb2_ecfp = self.apply_tokenizer(list(X.bb2_smiles))
+        X.bb3_ecfp = self.apply_tokenizer(list(X.bb3_smiles))
 
         # Print the vocabulary size of the tokenizer
-        self.log_to_terminal(f"the vocabulary size of the tokenizer {encoder.vocab_size}.")
+        self.log_to_terminal(f"the vocabulary size of the tokenizer {self.encoder.vocab_size}.")
 
         # Save the tokenizer as a torch file
-        with open(f"tm/{self.get_hash()}.pkl", 'wb') as f:
-            pickle.dump(encoder, f)
+        with open(f"tm/{self.get_hash()}.pkl", "wb") as f:
+            pickle.dump(self.encoder, f)
 
         return X, y
 
@@ -60,14 +72,18 @@ class TokenizerAtom(VerboseTrainingBlock):
         """Predict using the model.
 
         :param X: XData containing the molecule smiles
-        :return: the tokenized sentences"""
+        :return: the tokenized sentences
+        """
+        # Check whether the building blocks are present
+        if X.bb1_smiles is None or X.bb2_smiles is None or X.bb3_smiles is None:
+            raise ValueError("There is no SMILE information for the molecules")
 
         # Extract the tokenizer from the pickle file
-        encoder = joblib.load(f"tm/{self.get_hash()}.pkl")
+        self.encoder = joblib.load(f"tm/{self.get_hash()}.pkl")
 
         # Apply the torch tokenizer on each building block
-        X.bb1_smiles = self.apply_tokenizer(list(X.bb1_smiles), encoder)
-        X.bb2_smiles = self.apply_tokenizer(list(X.bb2_smiles), encoder)
-        X.bb3_smiles = self.apply_tokenizer(list(X.bb3_smiles), encoder)
+        X.bb1_ecfp = self.apply_tokenizer(list(X.bb1_smiles))
+        X.bb2_ecfp = self.apply_tokenizer(list(X.bb2_smiles))
+        X.bb3_ecfp = self.apply_tokenizer(list(X.bb3_smiles))
 
         return X
--- src/modules/transformation/segment_molecule.py
+++ src/modules/transformation/segment_molecule.py
@@ -1,20 +1,22 @@
 """Converts the molecule smiles into a sequence of consecutive tokens."""
 
-from dataclasses import dataclass
-from rdkit import Chem
-import re
-import numpy.typing as npt
-import numpy as np
-from tqdm import tqdm
-import joblib
-from src.typing.xdata import XData
 from concurrent.futures import ProcessPoolExecutor
+from dataclasses import dataclass
+
+import numpy as np
+import numpy.typing as npt
+from tqdm import tqdm
+
 from src.modules.transformation.verbose_transformation_block import VerboseTransformationBlock
+from src.typing.xdata import XData
+
 
 @dataclass
 class SegmentMolecule(VerboseTransformationBlock):
     """Converts the molecule smiles into a sequence of consecutive tokens.
-    param window_size: the size of each token in the sequence"""
+
+    param window_size: the size of each token in the sequence
+    """
 
     window_size: int = 6
     padding_size: int = 50
@@ -22,34 +24,36 @@ class SegmentMolecule(VerboseTransformationBlock):
 
     @staticmethod
     def segment_molecule(smiles: list[str], window_size: int, padding_size: int) -> list[list[str]]:
-        """perform the convolution operation on each smile
+        """Perform the convolution operation on each smile.
+
         param smiles: list containing the smile molecules
         """
         sequences = []
         for smile in smiles:
             # remove the branches from the smile
-            smile = smile.replace('(', '').replace(')', '')
+            new_smile = smile.replace("(", "").replace(")", "")
 
             # Extract n-grams from the sequence
-            length = len(smile) - window_size + 1
-            sequence = [" ".join(smile[i:i + window_size]) for i in range(length)]
+            length = len(new_smile) - window_size + 1
+            sequence = [" ".join(smile[i : i + window_size]) for i in range(length)]
 
             # Pad the sequence with special token
-            sequences.append(sequence + ['PAD'] * (padding_size - len(sequence)))
+            sequences.append(sequence + ["PAD"] * (padding_size - len(sequence)))
 
         return sequences
-    def parallel_segment(self, smiles: list[str],padding_size: int) -> npt.NDArray[np.str_]:
-        """ perform the convolution operation using multiprocessing.
-        param smiles: list containing the smile molecules"""
 
+    def parallel_segment(self, smiles: list[str], padding_size: int) -> npt.NDArray[np.str_]:
+        """Perform the convolution operation using multiprocessing.
+
+        param smiles: list containing the smile molecules
+        """
         # Divide the smiles molecules into chunks
-        chunks = [smiles[i: i + self.chunk_size] for i in range(0, len(smiles), self.chunk_size)]
+        chunks = [smiles[i : i + self.chunk_size] for i in range(0, len(smiles), self.chunk_size)]
 
         # Initialize the multiprocessing with the chunks
         results = []
         with ProcessPoolExecutor() as executor:
-            futures = [executor.submit(self.segment_molecule, smiles=chunk, window_size=self.window_size,
-                                       padding_size= padding_size) for chunk in chunks]
+            futures = [executor.submit(self.segment_molecule, smiles=chunk, window_size=self.window_size, padding_size=padding_size) for chunk in chunks]
 
             # Perform the multiprocessing on the chunks
             desc = "Perform convolution on the smiles"
@@ -57,19 +61,19 @@ class SegmentMolecule(VerboseTransformationBlock):
                 results.extend(future.result())
 
         return np.array(results)
+
     def custom_transform(self, data: XData) -> XData:
         """Compute the embeddings of the molecules in training.
+
         param data: the training or test set
         """
-
         # Check whether the building blocks are present
-        if data.bb1_smiles is None and data.bb2_smiles is None and data.bb3_smiles is None:
+        if data.bb1_smiles is None or data.bb2_smiles is None or data.bb3_smiles is None:
             raise ValueError("There is no SMILE information for the molecules")
 
         # Perform the convolutional operation on each block
-        data.bb1_smiles = self.parallel_segment(list(data.bb1_smiles),60)
+        data.bb1_smiles = self.parallel_segment(list(data.bb1_smiles), 60)
         data.bb2_smiles = self.parallel_segment(list(data.bb2_smiles), 40)
         data.bb3_smiles = self.parallel_segment(list(data.bb3_smiles), 50)
 
         return data
-
