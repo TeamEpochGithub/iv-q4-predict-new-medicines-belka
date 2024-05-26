15f5dfdf07c5678791cd907eb4d6fbe7dbacdaae
Ariel Ebersberger <ariel@ebersberger.io>
2024-05-26 19:12:36 +0200

    Fix cv.py

--- cv.py
+++ cv.py
@@ -1,15 +1,14 @@
 """The main script for Cross Validation. Takes in the raw data, does CV and logs the results."""
-import gc
 import os
-import time
 import warnings
 from contextlib import nullcontext
 from pathlib import Path
 from typing import Any
 
+import coloredlogs
 import hydra
 import numpy as np
-import polars as pl
+import numpy.typing as npt
 import randomname
 import wandb
 from epochalyst.logging.section_separator import print_section_separator
@@ -18,18 +17,20 @@ from hydra.utils import instantiate
 from omegaconf import DictConfig
 
 from src.config.cv_config import CVConfig
-from src.scoring.scorer import Scorer
-from src.setup.setup_data import setup_train_x_data, setup_train_y_data
+from src.setup.setup_data import GetXCache, GetYCache, setup_xy
 from src.setup.setup_pipeline import setup_pipeline
-from src.setup.setup_runtime_args import setup_cache_args, setup_train_args
+from src.setup.setup_runtime_args import create_cache_path, setup_cache_args, setup_train_args
 from src.setup.setup_wandb import setup_wandb
+from src.splitter.base import Splitter
+from src.typing.xdata import XData, slice_copy
 from src.utils.lock import Lock
 from src.utils.logger import logger
 from src.utils.set_torch_seed import set_torch_seed
 
+# Set logging
 warnings.filterwarnings("ignore", category=UserWarning)
-# Makes hydra give full error messages
-os.environ["HYDRA_FULL_ERROR"] = "1"
+os.environ["HYDRA_FULL_ERROR"] = "1"  # Makes hydra give full error messages
+
 # Set up the config store, necessary for type checking of config yaml
 cs = ConfigStore.instance()
 cs.store(name="base_cv", node=CVConfig)
@@ -38,6 +39,9 @@ cs.store(name="base_cv", node=CVConfig)
 @hydra.main(version_base=None, config_path="conf", config_name="cv")
 def run_cv(cfg: DictConfig) -> None:  # TODO(Jeffrey): Use CVConfig instead of DictConfig
     """Do cv on a model pipeline with K fold split. Entry point for Hydra which loads the config file."""
+    # Install coloredlogs
+    coloredlogs.install()
+
     # Run the cv config with an optional lock
     optional_lock = Lock if not cfg.allow_multiple_instances else nullcontext
     with optional_lock():
@@ -48,17 +52,11 @@ def run_cv_cfg(cfg: DictConfig) -> None:
     """Do cv on a model pipeline with K fold split."""
     print_section_separator("Q4 - Detect Medicine - CV")
 
-    import coloredlogs
-
-    coloredlogs.install()
-
     # Set seed
     set_torch_seed()
 
-    # Get output directory
+    # Set up Weights & Biases
     output_dir = Path(hydra.core.hydra_config.HydraConfig.get().runtime.output_dir)
-
-    # Set up Weights & Biases group name
     if cfg.wandb.enabled:
         wandb_group_name = randomname.get_name()
         setup_wandb(cfg, "cv", output_dir, name=wandb_group_name, group=wandb_group_name)
@@ -67,60 +65,85 @@ def run_cv_cfg(cfg: DictConfig) -> None:
     print_section_separator("Setup pipeline")
     model_pipeline = setup_pipeline(cfg)
 
-    # Cache arguments for x_sys and y_sys
-    processed_data_path = Path(cfg.processed_path) / f"{cfg.sample_size:_}{'_val' if cfg.val_split else ''}"
-    cache_args_x, cache_args_y, cache_args_train = setup_cache_args(processed_data_path)
+    # Setup cache arguments
+    cache_path = create_cache_path(cfg.cache_path, cfg.splitter, cfg.sample_size, cfg.sample_split)
+    splitter_cache_path = cache_path / "splits.pkl"
+    cache_args_x, cache_args_y, cache_args_train = setup_cache_args(cache_path)
 
-    # Check if the data is cached
-    x_cache_exists = model_pipeline.get_x_cache_exists(cache_args_x)
-    # y_cache_exists = model_pipeline.get_y_cache_exists(cache_args)
+    # Setup the data
+    X: XData | None = None
+    y: npt.NDArray[np.int_] | None = None
+    splits: list[tuple[npt.NDArray[np.int64], npt.NDArray[np.int64]]]
+    test_indices: npt.NDArray[np.int64] | None = None
 
-    directory = Path(cfg.data_path)
+    # Check if the data is cached and load if not
+    if (
+        not model_pipeline.get_x_cache_exists(cache_args_x)
+        or not model_pipeline.get_y_cache_exists(cache_args_y)
+        or (cfg.splitter is not None and not splitter_cache_path.exists())
+    ):
+        X, y = setup_xy(cfg)
 
-    first_time = time.time()
-    train_data = pl.read_parquet(directory / "train.parquet")
-    train_data = train_data.to_pandas(use_pyarrow_extension_array=True)
+    # Split the data into train and, optionally, test
+    if cfg.splitter is None:
+        raise ValueError("Splitter is required for cross validation.")
+    splitter: Splitter = instantiate(cfg.splitter)
+    if splitter.includes_validation:
+        logger.info("Splitting data into train, validation and test sets.")
+        splits, train_validation_indices, test_indices = splitter.split(X=X, y=y, cache_path=splitter_cache_path)  # type: ignore[assignment]
+    else:
+        logger.info("Splitting Data into train and validation sets.")
+        splits = splitter.split(X=X, y=y, cache_path=splitter_cache_path)  # type: ignore[assignment]
 
-    X, y = None, None
-    if not x_cache_exists:
-        X = setup_train_x_data(directory, train_data)
+    # Create Score arrays
+    with GetYCache(model_pipeline, cache_args_y, y) as y:
+        validation_scores = []
+        test_scores = []
+        combined_scores = []
+        oof_predictions = np.zeros(y.shape, dtype=np.float64)
 
-    y = setup_train_y_data(train_data)
-    del train_data
-    gc.collect()
-    logger.info(f"Total time:{time.time() - first_time}")
+    # Run Folds
+    for fold_no, (train_indices, validation_indices) in enumerate(splits):
+        validation_score, test_score, predictions = run_fold(
+            fold_no,
+            X,
+            y,
+            train_indices,
+            validation_indices,
+            test_indices,
+            cfg,
+            output_dir,
+            cache_args_x,
+            cache_args_y,
+            cache_args_train,
+        )
+        validation_scores.append(validation_score)
+        test_scores.append(test_score)
+        combined_scores.append((test_score + validation_score) / 2)
+        oof_predictions[validation_indices] = predictions
 
-    # Instantiate scorer
-    scorer = instantiate(cfg.scorer)
-    scores: list[float] = []
+    # Average Scores
+    with GetYCache(model_pipeline, cache_args_y, y) as y:
+        avg_val_score = np.average(np.array(validation_scores))
+        avg_test_score = np.average(np.array(test_scores))
+        avg_combined_score = np.average(np.array(combined_scores))
+        oof_score = instantiate(cfg.scorer)(y[train_validation_indices], oof_predictions[train_validation_indices])
 
-    # Split indices into train and test
-    # splitter_data = setup_splitter_data()
-    logger.info("Using splitter to split data into train and test sets.")
-
-    if not isinstance(y, np.ndarray):
-        raise TypeError("y should be a numpy array")
-
-    oof_predictions = np.zeros(y.shape, dtype=np.float64)
-
-    for fold_no, (train_indices, test_indices) in enumerate(instantiate(cfg.splitter).split(y)):
-        score, predictions = run_fold(fold_no, X, y, train_indices, test_indices, cfg, scorer, output_dir, cache_args_x, cache_args_y, cache_args_train)
-        scores.append(score)
-
-        # Save predictions
-        oof_predictions[test_indices] = predictions
-
-    avg_score = np.average(np.array(scores))
-    oof_score = scorer(y, oof_predictions)
-
+    # Report Scores
     print_section_separator("CV - Results")
-    logger.info(f"Avg Score: {avg_score}")
-    wandb.log({"Avg Score": avg_score})
-
+    logger.info(f"Avg Val Score: {avg_val_score}")
+    logger.info(f"Avg Test Score: {avg_test_score}")
+    logger.info(f"Avg Combined Score: {avg_combined_score}")
     logger.info(f"OOF Score: {oof_score}")
-    wandb.log({"OOF Score": oof_score})
-
-    logger.info("Finishing wandb run")
+    if wandb.run:
+        wandb.log(
+            {
+                "Validation Score": avg_val_score,
+                "Test Score": avg_test_score,
+                "Combined Score": avg_combined_score,
+                "OOF Score": oof_score,
+            },
+        )
     wandb.finish()
 
 
@@ -128,15 +151,15 @@ def run_fold(
     fold_no: int,
     X: Any,  # noqa: ANN401
     y: Any,  # noqa: ANN401
-    train_indices: list[int],
-    test_indices: list[int],
+    train_indices: npt.NDArray[np.int_],
+    validation_indices: npt.NDArray[np.int_],
+    test_indices: npt.NDArray[np.int_] | None,
     cfg: DictConfig,
-    scorer: Scorer,
-    output_dir: Path,
+    _output_dir: Path,
     cache_args_x: dict[str, Any],
     cache_args_y: dict[str, Any],
     cache_args_train: dict[str, Any],
-) -> tuple[float, Any]:
+) -> tuple[float, float, npt.NDArray[np.int_]]:
     """Run a single fold of the cross validation.
 
     :param i: The fold number.
@@ -152,36 +175,53 @@ def run_fold(
     """
     # Print section separator
     print_section_separator(f"CV - Fold {fold_no}")
-    logger.info(f"Train/Test size: {len(train_indices)}/{len(test_indices)}")
 
+    # Train Model
+    logger.info(f"Train/Test size: {len(train_indices)}/{len(validation_indices)}")
     logger.info("Creating clean pipeline for this fold")
     model_pipeline = setup_pipeline(cfg)
-
     train_args = setup_train_args(
         pipeline=model_pipeline,
         cache_args_x=cache_args_x,
         cache_args_y=cache_args_y,
         cache_args_train=cache_args_train,
         train_indices=train_indices,
-        test_indices=test_indices,
+        validation_indices=validation_indices,
         fold=fold_no,
         save_model=cfg.save_folds,
     )
-    predictions, _ = model_pipeline.train(X, y, **train_args)
+    validation_predictions, _ = model_pipeline.train(X, y, **train_args)
 
-    # If predictions are on 'all' data, only keep the test data
-    if y.shape[0] == predictions.shape[0]:
-        predictions = predictions[test_indices]
-    score = scorer(y[test_indices], predictions)
+    # Get Validation Score
+    with GetYCache(model_pipeline, cache_args_y, y) as y:
+        validation_score = instantiate(cfg.scorer)(y[validation_indices], validation_predictions)
 
-    logger.info(f"Score, fold {fold_no}: {score}")
+    # Get the test score
+    test_score = -1.0
+    combined_score = -1.0
+    if test_indices is not None:
+        with GetXCache(model_pipeline, cache_args_x, X) as X, GetYCache(model_pipeline, cache_args_y, y) as y:
+            X_sliced = slice_copy(X, test_indices)
+            test_predictions = model_pipeline.predict(X_sliced)
+            test_score = instantiate(cfg.scorer)(y[test_indices], test_predictions)
+            combined_score = 0.5 * validation_score + 0.5 * test_score
+            del X_sliced
 
-    fold_dir = output_dir / str(fold_no)  # Files specific to a run can be saved here
-    logger.debug(f"Output Directory: {fold_dir}")
+    # Setup Fold Folder
+    # fold_dir = output_dir / str(fold_no)  # Files specific to a run can be saved here
+    # logger.debug(f"Output Directory: {fold_dir}")
 
+    # Report Scores
+    logger.info(f"Score, fold {fold_no} - Val: {validation_score:.4f}, Test: {test_score:.4f}")
     if wandb.run:
-        wandb.log({f"Score_{fold_no}": score})
-    return score, predictions
+        wandb.log(
+            {
+                f"Validation Score - {fold_no}": validation_score,
+                f"Test Score - {fold_no}": test_score,
+                f"Combined Score - {fold_no}": combined_score,
+            },
+        )
+    return validation_score, test_score, validation_predictions
 
 
 if __name__ == "__main__":
