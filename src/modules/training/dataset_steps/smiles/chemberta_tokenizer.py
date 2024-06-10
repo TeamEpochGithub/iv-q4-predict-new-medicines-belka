0c1cf7b139a95749e7a2cdbfaf56e773334757fc
Gregoire <dumont.gregoire.c@gmail.com>
2024-06-10 15:26:54 +0200

    Fixed pre-commit issues

--- src/modules/training/dataset_steps/smiles/chemberta_tokenizer.py
+++ src/modules/training/dataset_steps/smiles/chemberta_tokenizer.py
@@ -2,11 +2,12 @@
 
 from dataclasses import dataclass
 from typing import Any
-from transformers import AutoTokenizer
+
 import numpy as np
 import numpy.typing as npt
 from epochalyst._core._caching._cacher import CacheArgs
 from epochalyst.pipeline.model.training.training_block import TrainingBlock
+from transformers import AutoTokenizer  # type: ignore[import-not-found]
 
 
 @dataclass
@@ -14,33 +15,30 @@ class ChembertaTokenizer(TrainingBlock):
     """Module to tokenize the molecule for chemberta.yaml model.
 
     :param model_name: the name of the Hugging Face model
-    :param padding_size: the maximum length to pad to"""
+    :param padding_size: the maximum length to pad to
+    """
 
     model_name: str = "DeepChem/ChemBERTa-10M-MTR"
     padding_size: int = 150
-    embedding_layer: bool = False
 
     def train(
-            self,
-            x: npt.NDArray[np.str_],
-            y: npt.NDArray[np.uint8],
-            _cache_args: CacheArgs | None = None,
-            **train_args: Any,
+        self,
+        x: npt.NDArray[np.str_],
+        y: npt.NDArray[np.uint8],
+        _cache_args: CacheArgs | None = None,
+        **train_args: Any,
     ) -> tuple[npt.NDArray[np.uint8], npt.NDArray[np.uint8]]:
         """Convert the molecule smiles into a sequence of integers.
 
         :param x: array containing the molecule smiles
-        :param y: array containing the protein labels"""
-
+        :param y: array containing the protein labels
+        """
         tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        output = tokenizer(list(x), truncation=True, padding='max_length', max_length=self.padding_size)
+        output = tokenizer(list(x), truncation=True, padding="max_length", max_length=self.padding_size)
 
-        if self.embedding_layer:
-            return np.array(output['input_ids']), y
-        else:
-            return output, y
+        return np.array(output["input_ids"]), y
+
     @property
     def is_augmentation(self) -> bool:
         """Check if augmentation is enabled."""
         return False
-
--- src/modules/training/models/chemberta.py
+++ src/modules/training/models/chemberta.py
@@ -1,10 +1,12 @@
-"""Module containing chemberta.yaml class copied from a  public notebook."""
-from transformers import AutoConfig, AutoTokenizer, AutoModel, DataCollatorWithPadding
+"""Module containing chemberta class copied from a  public notebook."""
 from torch import Tensor, nn
+from transformers import AutoConfig, AutoModel  # type: ignore[import-not-found]
+
 
 class Chemberta(nn.Module):
     """Pre-trained Hugging Face model for molecule smiles."""
-    def __init__(self, n_classes: int, model_name: str):
+
+    def __init__(self, n_classes: int, model_name: str) -> None:
         """Initialize the Hugging Face Model.
 
         :param model_name: the name of the Hugging Face model
@@ -15,12 +17,12 @@ class Chemberta(nn.Module):
         self.lm = AutoModel.from_pretrained(model_name, add_pooling_layer=False)
         self.dropout = nn.Dropout(self.config.hidden_dropout_prob)
         self.classifier = nn.Linear(self.config.hidden_size, self.config.num_labels)
-    def forward(self, x):
+
+    def forward(self, x: Tensor) -> Tensor:
         """Perform forward propagation of the model.
 
         :param x: the input data
         :return: the output data
         """
         hidden_state = self.lm(x.long()).last_hidden_state
-        # hidden_state = self.lm(x['input_ids'], attention_mask=x["attention_mask"]).last_hidden_state
         return self.classifier(self.dropout(hidden_state[:, 0]))
