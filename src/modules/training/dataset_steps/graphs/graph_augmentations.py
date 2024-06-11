65504d8726ba8cb9577898b5e86ba98a4b534afb
Daniel De Dios <D.deDiosAllegue-1@student.tudelft.nl>
2024-06-11 14:08:24 +0200

    Adds graph augmentations and fixes graph dataset step

--- src/modules/training/dataset_steps/graphs/graph_augmentations.py
+++ src/modules/training/dataset_steps/graphs/graph_augmentations.py
@@ -1,20 +1,17 @@
-""" Graph Augmentations Class. """
+"""Graph Augmentations Class."""
 from dataclasses import dataclass
-from random import random
-from typing import Tuple, List, Any
+from typing import Any
 
 import numpy as np
-from epochalyst.pipeline.model.training.training_block import TrainingBlock
 import numpy.typing as npt
-from rdkit import Chem
-from rdkit.Chem import rdchem
 import torch
+from epochalyst.pipeline.model.training.training_block import TrainingBlock
 from torch_geometric.data import Data
 
 
 @dataclass
 class GraphAugmentationBlock(TrainingBlock):
-    """ Graph Augmentations Block. """
+    """Graph Augmentations Block."""
 
     p_node_drop: float = 0.1
     p_edge_drop: float = 0.1
@@ -48,10 +45,10 @@ class GraphAugmentationBlock(TrainingBlock):
         # Remap edge indices
         edge_index = node_map[edge_index]
 
-        return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y = data.y)
+        return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=data.y)
 
     def apply_mask(self, data: Data) -> Data:
-        """ Applies mask to the nodes and edge attributes"""
+        """Apply mask to the nodes and edge attributes."""
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
 
         # Node attribute masking
@@ -68,10 +65,10 @@ class GraphAugmentationBlock(TrainingBlock):
 
     def train(
         self,
-        x: List[Data],
+        x: list[Data],
         y: npt.NDArray[np.uint8] | None = None,
         **train_args: Any,
-    ) -> Tuple[List[Data], None]:
+    ) -> tuple[list[Data], npt.NDArray[np.uint8] | None]:
         """Apply augmentations and return augmented Data objects.
 
         :param x: The x molecule data in Data format
--- src/modules/training/models/gnn_transformer_conv.py
+++ src/modules/training/models/gnn_transformer_conv.py
@@ -1,11 +1,11 @@
 """GCN with Transformer Convolutions Module."""
 
-import numpy as np
 import torch
 from torch import nn
 from torch_geometric.data import Data
 from torch_geometric.nn import TransformerConv, global_mean_pool
 
+from src.modules.training.dataset_steps.graphs.smiles_to_graph import unpack_atom_features, unpack_edge_features
 
 
 class GNNTransformerModel(torch.nn.Module):
@@ -16,7 +16,7 @@ class GNNTransformerModel(torch.nn.Module):
     :param n_classes:  Number of classes to predict
     """
 
-    def __init__(self, num_node_features: int, num_edge_features: int, n_classes: int, hidden_dim: int = 32, out_features: int = 1024, dropout : float = 0.1) -> None:
+    def __init__(self, num_node_features: int, num_edge_features: int, n_classes: int, hidden_dim: int = 32, out_features: int = 1024, dropout: float = 0.1) -> None:
         """Initialize the GCN model.
 
         :param num_node_features: Number of features per node
@@ -50,7 +50,9 @@ class GNNTransformerModel(torch.nn.Module):
         """
         x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
 
-        edge_attr = unpack_bits(edge_attr.to(torch.uint8)).float()
+        # Unpack Node and Edge Attributes
+        x = unpack_atom_features(x).to(torch.float)
+        edge_attr = unpack_edge_features(edge_attr).to(torch.float)
 
         x = self.relu(self.conv1(x, edge_index, edge_attr))
         x = self.relu(self.conv2(x, edge_index, edge_attr))
@@ -67,50 +69,3 @@ class GNNTransformerModel(torch.nn.Module):
         x = self.dropout3(x)
 
         return self.fc4(x)
-
-def unpack_bits(tensor: torch.Tensor, dim: int = -1, mask: int = 0b00000001, dtype: torch.dtype = torch.uint8) -> torch.Tensor:
-    """Unpack bits tensor into bits tensor."""
-    return f_unpackbits(tensor, dim=dim, mask=mask, dtype=dtype)
-
-
-def f_unpackbits(
-    tensor: torch.Tensor,
-    dim: int = -1,
-    mask: int = 0b00000001,
-    shape: tuple[int, ...] | None = None,
-    out: torch.Tensor | None = None,
-    dtype: torch.dtype = torch.uint8,
-) -> torch.Tensor:
-    """Unpack bits tensor into bits tensor."""
-    dim = dim if dim >= 0 else dim + tensor.dim()
-    shape_, nibbles, nibble = packshape(tensor.shape, dim=dim, mask=mask, pack=False)
-    shape = shape if shape is not None else shape_
-    out = out if out is not None else torch.empty(shape, device=tensor.device, dtype=dtype)
-
-    if shape[dim] % nibbles == 0:
-        shift = torch.arange((nibbles - 1) * nibble, -1, -nibble, dtype=torch.uint8, device=tensor.device)
-        shift = shift.view(nibbles, *((1,) * (tensor.dim() - dim - 1)))
-        return torch.bitwise_and((tensor.unsqueeze(1 + dim) >> shift).view_as(out), mask, out=out)
-
-    for i in range(nibbles):
-        shift = nibble * i  # type: ignore[assignment]
-        sliced_output = tensor_dim_slice(out, dim, slice(i, None, nibbles))
-        sliced_input = tensor.narrow(dim, 0, sliced_output.shape[dim])
-        torch.bitwise_and(sliced_input >> shift, mask, out=sliced_output)
-    return out
-
-
-def packshape(shape: tuple[int, ...], dim: int = -1, mask: int = 0b00000001, *, pack: bool = True) -> tuple[tuple[int, ...], int, int]:
-    """Define pack shape."""
-    dim = dim if dim >= 0 else dim + len(shape)
-    bits = 8
-    nibble = 1 if mask == 0b00000001 else 2 if mask == 0b00000011 else 4 if mask == 0b00001111 else 8 if mask == 0b11111111 else 0
-    nibbles = bits // nibble
-    shape = (shape[:dim] + (int(np.ceil(shape[dim] / nibbles)),) + shape[dim + 1 :]) if pack else (shape[:dim] + (shape[dim] * nibbles,) + shape[dim + 1 :])
-    return shape, nibbles, nibble
-
-
-def tensor_dim_slice(tensor: torch.Tensor, dim: int, dim_slice: slice) -> torch.Tensor:
-    """Slices a tensor for packing."""
-    return tensor[(dim if dim >= 0 else dim + tensor.dim()) * (slice(None),) + (dim_slice,)]
-
